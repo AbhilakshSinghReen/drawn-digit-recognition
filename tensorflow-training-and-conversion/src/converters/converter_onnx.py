5e64ea5b3312940b99a7adb2d0f3a77dadfc8d1d
AbhilakshSinghReen <abhilakshreen@gmail.com>
2024-04-20 16:56:22 +0530

    TensorFlow: added onnx converter.

--- tensorflow-training-and-conversion/src/converters/converter_onnx.py
+++ tensorflow-training-and-conversion/src/converters/converter_onnx.py
@@ -1,16 +1,20 @@
 from os.path import join as path_join
 
 from keras.models import load_model
+import onnx
 from tensorflowjs import converters
+import tf2onnx
 
 from ..config import models_dir
 
 
 MODEL_FILE_PATH = path_join(models_dir, "training_id", "epoch-epoch_number.model")
-TFJS_MODEL_FILE_PATH = MODEL_FILE_PATH[:-6] + "-tfjs"
+ONNX_MODEL_FILE_PATH = MODEL_FILE_PATH[:-6] + ".onnx"
 
 
 if __name__ == "__main__":
     keras_model = load_model(MODEL_FILE_PATH)
-    converters.save_keras_model(keras_model, TFJS_MODEL_FILE_PATH)
-    print(f"TensorFlow JS model saved at: {TFJS_MODEL_FILE_PATH}")
+    onnx_model, _ = tf2onnx.convert.from_keras(keras_model)
+    
+    onnx.save(onnx_model, ONNX_MODEL_FILE_PATH)
+    print(f"ONNX model saved at: {ONNX_MODEL_FILE_PATH}")
