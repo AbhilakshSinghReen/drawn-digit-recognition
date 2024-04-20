f985a69760aad01dd3fe5543e87d4aa2f3e4726d
AbhilakshSinghReen <abhilakshreen@gmail.com>
2024-04-20 19:49:39 +0530

    API completed

--- backends/fastapi-tensorflow/src/app.py
+++ backends/fastapi-tensorflow/src/app.py
@@ -3,6 +3,7 @@ from os.path import join as path_join
 import cv2
 from fastapi import FastAPI, File, UploadFile
 import numpy as np
+from tensorflow.keras.utils import normalize
 
 from .config import models_dir
 from .model import CNN
@@ -11,7 +12,6 @@ from .model import CNN
 app = FastAPI()
 
 MODEL_WEIGHTS_FILE_PATH = path_join(models_dir, "training_id", "epoch-epoch_number")
-MODEL_WEIGHTS_FILE_PATH = path_join(models_dir, "tensorflow---2024-04-20-18-35-16", "epoch-9.h5")
 
 model = CNN()
 model.load_weights(MODEL_WEIGHTS_FILE_PATH)
@@ -19,18 +19,16 @@ model.load_weights(MODEL_WEIGHTS_FILE_PATH)
 
 def preprocess_image(image):
     image = cv2.resize(image, (28, 28))
-    print(np.unique(image))
-    image = image.astype(np.float64)
-    image = image / 255
+    image = normalize(image, axis=1)
     image = np.array([image])
     return image
 
 
 @app.post("/api/run-inference")
 async def predict_image(file: UploadFile = File(...)):
-    contents = await file.read()
-    nparr = np.frombuffer(contents, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+    file_contents = await file.read()
+    np_arr = np.frombuffer(file_contents, np.uint8)
+    image = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
     
     preprocessed_image = preprocess_image(image)
 
--- backends/fastapi-tensorflow/tests/test_apis.py
+++ backends/fastapi-tensorflow/tests/test_apis.py
@@ -1,13 +1,18 @@
 import requests
+import os
 
 url = "http://localhost:8000/api/run-inference"
 
 image_file_path = "1.png"
 
-with open(image_file_path, "rb") as file:
-    response = requests.post(url, files={"file": file})
+for image_file_path in os.listdir(os.path.dirname(__file__)):
+    if not image_file_path.endswith(".png"):
+        continue
 
-if response.status_code == 200:
-    print("Predicted Label:", response.json()["predicted_label"])
-else:
-    print("Error:", response.text)
+    with open(image_file_path, "rb") as file:
+        response = requests.post(url, files={"file": file})
+
+    if response.status_code == 200:
+        print(f"Predicted Label: {image_file_path}, ", response.json()["predicted_label"])
+    else:
+        print("Error:", response.text)
