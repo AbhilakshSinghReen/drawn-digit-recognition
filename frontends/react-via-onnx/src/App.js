0f887dc4fb5fd64b85046e76d1719b175c08b748
AbhilakshSinghReen <abhilakshreen@gmail.com>
2024-05-06 06:15:59 +0530

    Cleaned up App.js for react-via-onnx frontend.

--- frontends/react-via-onnx/src/App.js
+++ frontends/react-via-onnx/src/App.js
@@ -1,32 +1,9 @@
 import Home from "./pages/Home";
 
-import { useEffect, useState } from "react";
-
 export default function App() {
-  // const [ort, setOrt] = useState(null);
-
-  // const loadWasm = async () => {
-  //   try {
-  //     const ort = await import("onnxruntime-web");
-  //     const modelUrl = process.env.PUBLIC_URL + "/model.onnx";
-  //     console.log(modelUrl);
-  //     const newOrtSession = await ort.InferenceSession.create(modelUrl);
-
-  //     setOrt(ort);
-  //     console.log("ort import ok");
-  //   } catch (err) {
-  //     console.error(`Unexpected error in loadWasm. [Message: ${err.message}]`);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   loadWasm();
-  // }, []);
-
   return (
     <div className="App">
       <Home />
-      {/* <h1>Hello World</h1> */}
     </div>
   );
 }
