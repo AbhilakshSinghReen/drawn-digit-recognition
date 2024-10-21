a5bb97a2e9b39a258e89f03ef005690762f3c4a0
AbhilakshSinghReen <abhilakshreen@gmail.com>
2024-10-21 18:20:34 +0530

    Cleanup and added tests.

--- backends/fastapi-onnx/foo/src/config.py
+++
@@ -1,4 +0,0 @@
-from os.path import dirname, join as path_join
-
-
-models_dir = path_join(dirname(dirname(dirname(dirname(__file__)))), "models")
---
+++ backends/fastapi-tensorflow/tests/config.py
@@ -0,0 +1,4 @@
+from os.path import dirname, join as path_join
+
+
+data_dir = path_join(dirname(dirname(__file__)), "data")
